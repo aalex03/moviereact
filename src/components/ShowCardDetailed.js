c390a7b80fb6aad9e489cfa3e94f7d492acb3ce8
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2023-12-29 03:04:45 +0200

    add show page placeholder

---
+++ src/components/ShowCardDetailed.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { ref, getDownloadURL } from "firebase/storage";
+import { storage } from "../firebaseApp";
+function ShowCardDetailed({ show }) {
+    const [imageUrl, setImageUrl] = useState(null);
+
+    useEffect(() => {
+        const imageRef = ref(storage, show.PosterURL);
+
+        getDownloadURL(imageRef)
+            .then(url => { console.log(url); setImageUrl(url) })
+            .catch(error => console.error('Error getting download URL:', error));
+    }, [show.PosterURL]);
+
+
+    return (
+        <Row>
+            <Col className="w-25">
+                <Card style={{ width: '18rem', height: "560px"}}>
+                    <Card.Title>{show.Title}</Card.Title>
+                    <Card.Img variant="top" src={imageUrl} />
+                    <Card.Body>
+                        <Card.Text style={{ fontSize: 'small', overflowY: 'auto', maxHeight: '100px' }}>
+
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        
+                    </Card.Footer>
+                </Card>
+            </Col>
+            <Col className="w-75 justify-content-center text-center">
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src={show.TrailerURL} allowFullScreen style={{height: "560px", width: "848px"}}></iframe>
+                </div>
+            </Col>
+        </Row>
+    );
+}
+
+export default ShowCardDetailed;
--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -2,25 +2,30 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {db} from "../firebaseApp";
 import {collection, getDocs, query, where} from "firebase/firestore";
+import ShowCardDetailed from "../components/ShowCardDetailed";
 
 function ShowPage()
 {
     const {title} = useParams();
-    var [show, setShow] = useState(null);
-
+    const [show, setShow] = useState(null);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         async function getShow() {
             const q = query(collection(db, "shows"), where("Title", "==", title));
             const querySnapshot = await getDocs(q);
             setShow(querySnapshot.docs[0].data());
+            setLoading(false);
         }
         getShow();
     }, [title]);
 
-    
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
     return (
         <div>
-            <h1>{title}</h1>
+            {show && <ShowCardDetailed show={show} />}
         </div>
     );
 }
