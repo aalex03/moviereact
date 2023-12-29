9625d3b04843390bb84fb8748babe1d75df0ce39
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2023-12-29 02:33:07 +0200

    found the correct way to get stuff from the firestore wow this took like an hour

--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import {db} from "../firebaseApp";
+import {collection, getDocs, query, where} from "firebase/firestore";
+
 function ShowPage()
 {
     const {title} = useParams();
+    var [show, setShow] = useState(null);
+
+    useEffect(() => {
+        async function getShow() {
+            const q = query(collection(db, "shows"), where("Title", "==", title));
+            const querySnapshot = await getDocs(q);
+            setShow(querySnapshot.docs[0].data());
+        }
+        getShow();
+    }, [title]);
+
+    
     return (
         <div>
             <h1>{title}</h1>
