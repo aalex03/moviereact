10d43e6e6e841b2800185ee9890fef327210813c
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-01-06 06:12:10 +0200

    prompt user to set up their profiles when logging in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { doc, getDoc } from 'firebase/firestore';
+import { collection, doc, getDoc } from 'firebase/firestore';
 import { auth, db } from './firebaseApp';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData, useDocument } from 'react-firebase-hooks/firestore';
 
 
 
@@ -11,19 +12,48 @@ import NavigationBar from './components/NavigationBar';
 import Shows from './pages/Shows';
 import UserShowList from './pages/UserShowList';
 import ShowPage from './pages/ShowPage';
+import HomePage from './pages/HomePage';
 import ProfileSetup from './components/ProfileSetup';
+
+const checkProfileExists = async (user) => {
+  const profileRef = doc(db, 'profiles', user.uid);
+  const profileDoc = await getDoc(profileRef);
+  return profileDoc.exists();
+};
+
 
 function App() {
   
   const [user] = useAuthState(auth);
+  const [profileExists, setProfileExists] = useState(null);
+  const profilesRef = collection(db, 'profiles');
+  const profiles = useCollectionData(profilesRef, {idField: 'id'});
+  useEffect(() => {
+    if (user) {
+      checkProfileExists(user).then((result) => setProfileExists(result));
+    }
+  }, [user, profiles]);
 
+  if (user && profileExists === null) {
+    return <div className='App'>
+      <div className='App-header'>Loading..</div>
+    </div>
+  }
+
+  if (user && !profileExists) {
+    return <div className='App'>
+      <div className='App-header'>
+        <ProfileSetup />  
+      </div>
+    </div>
+  }
   return (
     <div className="App">
       <header className="App-header">
         <Router>
-          <NavigationBar user={user} />
+        <NavigationBar user={user} />
           <Routes>
-            <Route path="/" element={<Shows/>} />
+            <Route path="/" element={<HomePage/>} />
             <Route path="/shows" element={<Shows/>} />
             <Route path="/showlist" element={<UserShowList/>} />
             <Route path="/showpage/:title" element={<ShowPage/>} />
--- src/components/ProfileSetup.js
+++ src/components/ProfileSetup.js
@@ -1,38 +1,45 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap'; // Assuming you're using react-bootstrap components
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { getAuth, updateProfile } from 'firebase/auth';
-import { auth } from '../firebaseApp';
-
+import { Form, Button } from 'react-bootstrap';
+import {doc, setDoc } from 'firebase/firestore';
+import {auth, db} from '../firebaseApp';
 const ProfileSetup = () => {
-  const [user] = useAuthState(auth);
-  const [newUsername, setNewUsername] = useState('');
+  const [username, setUsername] = useState('');
 
-  const handleSave = async () => {
-    try {
-      await updateProfile(auth.currentUser, { displayName: newUsername });
-      // Redirect to the main application or perform any necessary action
-    } catch (error) {
-      console.error('Error updating username:', error.message);
-      // Handle error (e.g., show an error message to the user)
+  const handleProfileSetup = async () => {
+    const user = auth.currentUser;
+
+    if (user) {
+      const profileRef = doc(db, 'profiles', user.uid);
+
+      try {
+        // Set profile data in the "profiles" collection
+        await setDoc(profileRef, {
+          username: username,
+        });
+
+        // Assume the profile setup is successful
+        console.log('Profile setup successful!');
+      } catch (error) {
+        console.error('Error setting up profile:', error);
+      }
     }
   };
 
   return (
-    <div className="container">
-      <h2>Set Up Your Profile</h2>
+    <div>
+      <h2>Profile Setup</h2>
       <Form>
         <Form.Group controlId="formUsername">
-          <Form.Label>Username</Form.Label>
+          <Form.Label>Username:</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter your username"
-            value={newUsername}
-            onChange={(e) => setNewUsername(e.target.value)}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </Form.Group>
-        <Button variant="primary" onClick={handleSave}>
-          Save
+        <Button variant="primary" onClick={handleProfileSetup}>
+          Set Up Profile
         </Button>
       </Form>
     </div>
