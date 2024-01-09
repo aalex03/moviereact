c6675bae213c7b1067310724b04fc4079587c33a
Razvan <razvan.litzemberger@student.upt.ro>
2024-01-10 00:11:46 +0200

    navbar text allignment

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -6,27 +6,32 @@ import Logout from './Logout';
 
 function NavigationBar({ userProfile }) {
   return (
-    <Navbar bg="dark" expand="lg" fixed='top' style={{ padding: '10px',  backgroundColor: '#1d2026' }}>
+    <Navbar bg="dark" expand="lg" fixed='top' style={{ padding: '10px', backgroundColor: '#1d2026' }}>
       <Navbar.Brand href="#home">MovieReact</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav>
+        <Nav className='me-auto'>
           <NavLink to={'/shows'} className='nav-link'>Shows</NavLink>
           {userProfile && (
-            <NavLink to={'/showlist'} className='nav-link'>My list</NavLink>
+            <NavLink to={'/showlist'} className='nav-link'>My List</NavLink>
           )}
-          
-        </Nav>
-        <Nav className='ms-auto'>
-        {userProfile && userProfile.isAdmin && (
+          {userProfile && userProfile.isAdmin && (
             <NavLink to={'/admin'} className='nav-link'>Admin</NavLink>
           )}
+        </Nav>
+        <Nav className='ms-auto'>
           <Form className='ms-2 me-2'>
-            <FormControl inline type="text" placeholder="Search" className="m-2" style={{backgroundColor: '#282c34',color:'#6a748b', '::placeholder': { color: '#6a748b' } }} />
+            <FormControl
+              inline
+              type="text"
+              placeholder="Search"
+              className="m-2"
+              style={{ backgroundColor: '#282c34', color: '#6a748b', '::placeholder': { color: '#6a748b' } }}
+            />
           </Form>
           <div className='ms-2 me-2'>
             {userProfile ? (
-              <div>
+              <div className='d-flex align-items-center'>
                 <small className='ms-2 me-2'>{userProfile.Username}</small>
                 <Logout />
               </div>
