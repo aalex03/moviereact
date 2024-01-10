b49528f05b76c89130ef10bcab0f520f0e00d4a3
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2024-01-10 17:53:53 +0200

    follow and unfollow shows

--- src/components/ShowCardDetailed.js
+++ src/components/ShowCardDetailed.js
@@ -1,22 +1,69 @@
 import { useState, useEffect } from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Card, Col, Container, Row, Button } from "react-bootstrap";
 import { ref, getDownloadURL } from "firebase/storage";
 import { storage, auth } from "../firebaseApp";
+import {doc, getDoc, deleteDoc, setDoc, addDoc, collection, query, getDocs, where} from 'firebase/firestore';
+import { db } from "../firebaseApp";
 
 function ShowCardDetailed({ showDoc }) {
-    
-    const show = showDoc.data();
-    const [imageUrl, setImageUrl] = useState(null);
 
+    const show = showDoc.data();
+    const [currentUser] = useAuthState(auth);
+    const [imageUrl, setImageUrl] = useState(null);
+    const [followStatus, setFollowStatus] = useState(false);
     useEffect(() => {
         const show = showDoc.data();
         const imageRef = ref(storage, show.PosterURL);
-
+        getFollowStatus();
         getDownloadURL(imageRef)
-            .then(url => {setImageUrl(url) })
+            .then(url => { setImageUrl(url) })
             .catch(error => console.error('Error getting download URL:', error));
     }, [show.PosterURL, showDoc]);
 
+    const getFollowStatus = async () => {
+        const userId = currentUser.uid;
+        const showId = showDoc.id;
+
+        const followStatus = await getFollowDocument(userId, showId);
+        setFollowStatus((followStatus != null));
+    }
+
+    const handleFollow = async () => {
+        const showId = showDoc.id;
+        const userId = currentUser.uid;
+
+        const followDoc = await getFollowDocument(userId, showId);
+        if (followDoc != null)
+        {
+            await deleteDoc(followDoc);
+            setFollowStatus(false);
+        }
+        else
+        {
+            await addDoc(collection(db,'follows'), {UserId: userId, ShowId: showId});
+            setFollowStatus(true);
+        }
+    }
+
+    async function getFollowDocument(userId, showId) {
+        const q = query(
+          collection(db, 'follows'),
+          where('UserId', '==', userId),
+          where('ShowId', '==', showId)
+        );
+      
+        const querySnapshot = await getDocs(q);
+      
+        if (!querySnapshot.empty) {
+          // Document found
+          const doc = querySnapshot.docs[0].ref;
+          return doc;
+        } else {
+          // No document found
+          return null;
+        }
+      }
 
     return (
         <div>
@@ -26,9 +73,12 @@ function ShowCardDetailed({ showDoc }) {
                         <Card.Title>{show.Title}</Card.Title>
                         <Card.Img variant="top" src={imageUrl} />
                         <Card.Body>
-                            <Card.Text style={{ fontSize: 'small', overflowY: 'auto', maxHeight: '100px' }}>
-
+                            <Card.Text style={{ fontSize: 'small', overflowY: 'auto', maxHeight: '100px', textAlign: 'left' }}>
+                                Rating: {show.Rating}⭐
+                                <br />
+                                Status: {show.Status}
                             </Card.Text>
+                            <Button variant="info" onClick={handleFollow}>{followStatus ? 'Unfollow' : 'Follow'}</Button>
                         </Card.Body>
                         <Card.Footer>
 
